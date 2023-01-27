87a107be463aff7fa52074ae83dfdbec0f68c7c1
Miguel Angel Gonzalez <magonzalez112@users.noreply.github.com>
2023-01-27 13:13:01 +0100

    Update zabbix_mtr.sh
    Commented one line in the script. Maybe it is useful in older versions of mtr but in newer versions when it cannot contact with a host it prints 0.0 value, so it is useless

--- zabbix_mtr.sh
+++ zabbix_mtr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wrst=$(mtr -r $* | grep -v HOST | tail -n 1 | awk '{ print ($8) }')
-wrst=${wrst:-"-1.0"}
+#wrst=${wrst:-"-1.0"} This does literally nothing in Kali Linux 21
 echo $wrst
